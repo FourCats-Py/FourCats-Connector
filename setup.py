bc55090ebbcaa2de8609f1893b391ac6f9b443c0
石伟东 <>
2022-04-12 19:10:14 +0800

    Commit: fix: Modify packaging script

--- setup.py
+++ setup.py
@@ -2,13 +2,32 @@
 # -*- coding: UTF-8 -*-
 
 # TIME ： 2022-04-12
-from setuptools import setup, find_packages
+import re
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open("fourcats_connector/__init__.py", "r") as file:
+    regex_version = r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]'
+    version = re.search(regex_version, file.read(), re.MULTILINE).group(1)
+
+with open("README.md", "rb") as file:
+    readme = file.read().decode("utf-8")
+
 
 setup(
     name="fourcats-connector",
-    version="0.0.1",
+    version=version,
+    packages=["fourcats_connector"],
     description="A quick use tool for Python common connectors.",
-    packages=find_packages(),
-    install_requires=["certifi>=2021.10.8", "loguru>=0.6.0"],
-    long_description="""A quick use tool for Python common connectors."""
+    long_description=readme,
+    long_description_content_type="text/x-rst",
+    author="ShiWeiDong",
+    url="https://github.com/FourCats-Py/FourCats-Connector",
+    download_url="https://github.com/Delgan/loguru/archive/{}.tar.gz".format(version),
+    keywords=["fourcats", "connect", "connector"],
+    install_requires=["loguru>=0.6.0"],
+    python_requires=">=3.5"
 )
